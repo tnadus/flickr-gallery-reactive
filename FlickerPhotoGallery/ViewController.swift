1b2eb800baecc967fb25dcc5a02461614e331a3c
Murat Sudan <tarumnadus@gmail.com>
2017-08-06 01:21:23 +0100

    Initial Commit

---
+++ FlickerPhotoGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickerPhotoGallery
+//
+//  Created by Murat Sudan on 06/08/2017.
+//  Copyright © 2017 Murat Sudan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
